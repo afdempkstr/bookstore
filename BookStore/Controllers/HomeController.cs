ce0d7055f8b32e8cb2336d7b0235e4f5681857ba
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-03-06 01:54:46 +0200

    Added layout for views

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
