7a065d254803627a269f36a02f87fe47446208fb
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-03-06 03:01:46 +0200

    Add fluentmigrator and migrations for book and publisher models

---
+++ BookStore/Migrations/AddBookTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace BookStore.Migrations
+{
+    [Migration(20190306020000)]
+    public class AddBookTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Book")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Title").AsString(50).NotNullable().Indexed()
+                .WithColumn("Author").AsString(50).NotNullable().Indexed()
+                .WithColumn("CoverPhoto").AsString(255)
+                .WithColumn("PublicationYear").AsInt32().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Book");
+        }
+    }
+}
---
+++ BookStore/Migrations/AddPublisherTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace BookStore.Migrations
+{
+    [Migration(20190306010000)]
+    public class AddPublisherTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Publisher")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(50).NotNullable().Indexed();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Publisher");
+        }
+    }
+}
