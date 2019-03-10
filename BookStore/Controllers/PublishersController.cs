01ad893d47814d09a36240f6a1eda6a05b8b1bd8
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-03-11 00:58:51 +0200

    Added index view for publishers

---
+++ BookStore/Controllers/PublishersController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Domain.Models;
+using BookStore.Repositories;
+
+namespace BookStore.Controllers
+{
+    public class PublishersController : Controller
+    {
+        // GET: Publisher
+        public ActionResult Index()
+        {
+            var publishers = Enumerable.Empty<Publisher>();
+            using (var db = new BookStoreDb())
+            {
+                publishers = db.Publishers.All();
+            }
+
+            return View(publishers);
+        }
+
+        // GET: Publisher/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Publisher/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Publisher/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Publisher/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Publisher/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Publisher/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Publisher/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
