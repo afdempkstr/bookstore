fd3b8f53c13a6aca0c35ac2edb114214e8b98387
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-03-20 01:44:13 +0200

    Add user table and user role table migrations

---
+++ BookStore/Migrations/AddUserRolesTable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+
+namespace BookStore.Migrations
+{
+    [Migration(20190320010000)]
+    public class AddUserRolesTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("UserRoles")
+                .WithColumn("UserId").AsInt32().NotNullable()
+                .ForeignKey("FK_UserRoles_User", "User", "Id")
+                .OnDeleteOrUpdate(Rule.Cascade)
+                .WithColumn("RoleId").AsInt32().NotNullable()
+                .ForeignKey("FK_UserRoles_Role", "Role", "Id")
+                .OnDeleteOrUpdate(Rule.Cascade);
+
+            Create.PrimaryKey().OnTable("UserRoles").Columns("UserId", "RoleId");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("UserRoles");
+        }
+    }
+}
---
+++ BookStore/Migrations/AddUserTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+
+namespace BookStore.Migrations
+{
+    [Migration(20190319010000)]
+    public class AddUserTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("User")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Username").AsString(50).NotNullable().Unique()
+                .WithColumn("Name").AsString(50).NotNullable()
+                .WithColumn("Password").AsString(255).NotNullable()
+                .WithColumn("Salt").AsString(20).NotNullable()
+                .WithColumn("RegisteredAt").AsDateTimeOffset().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("User");
+        }
+    }
+}
