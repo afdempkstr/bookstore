86732fe64765df4c959c06960081d72faec598e8
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-04-01 15:17:04 +0300

    cleanup

--- BookStore/NotificationHub.cs
+++ BookStore/NotificationHub.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BookStore.Application;
 using BookStore.Domain.Application;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
 
 namespace BookStore
 {
     public class NotificationHub : Hub<IBookStoreAppClient>
     {
+        private IBookStoreApp _app;
+
+        public NotificationHub(IBookStoreApp app)
+        {
+            _app = app;
+        }
+
         public void Hello()
         {
             Clients.All.OnNotify("hello");
+        }
+
+        public override async Task OnConnected()
+        {
+            _app.GetPublisher(1);
+            await base.OnConnected();
         }
     }
 }
